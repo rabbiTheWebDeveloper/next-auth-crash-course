946aa36cb47bd2a83af988d6f17c16e5e9f67e61
S M AL RABBI <rabbithedeveloper@gmail.com>
2024-05-11 16:15:17 +0600

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,36 @@
+import { auth } from "@/auth";
+import { doSignIn, doSignout } from "./action";
 import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+
+export default async function Home() {
+  const session=await auth();
+  console.log(session)
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <section>
+      {
+        !session && <p>Not signed in</p>
+      }
+      <h1>Hello World</h1>
+
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+      {
+        session?.user && <p>hello {session.user.name}</p>
+      }
+      <div className="rounded-full ">
+        <img  src={session?.user?.image} alt="Vercel Logo" width={72} height={16} />
       </div>
+      <div className="flex gap-2">
+      <form action={doSignIn}>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Google Signin</button>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      </form>
+      <form action={doSignout}>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
+
+      </form>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  
+    </section>
   );
 }
---
+++ src/auth.js
@@ -0,0 +1,16 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import mongoClientPromise from "./lib/mongoClinetPromise";
+
+export const { handlers :{GET , POST}, signIn, signOut, auth } = NextAuth({
+  adapter: MongoDBAdapter(mongoClientPromise , {databaseName : process.env.ENVIRONMENT}),
+  providers: [
+    GoogleProvider(
+      {
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      }
+    )
+  ],
+});
